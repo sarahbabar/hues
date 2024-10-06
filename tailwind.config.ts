a3c3f63a9de7ee97b19cf839c7304a8cb51f24dd
Sarah Babar <59743057+sarahbabar@users.noreply.github.com>
2024-10-06 04:58:49 -0400

    added global styles and custom font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+        fontFamily: {
+          departure: ["departure-mono"],
+        } 
     },
   },
   plugins: [],
